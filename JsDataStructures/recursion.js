9578557be9300f45d1b1a4584b1a62a08921f5a0
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-03-28 23:32:27 +0400

    jsdata

---
+++ JsDataStructures/recursion.js
@@ -0,0 +1,5 @@
+(function() {
+    // sadəcə burda kod yazmağınıza icaze verilir
+    // teleb olunan 1-100 arasındakı ədədlərin ekrana cap edilməsini təmin etməkdir
+    // while for və digər loop expressionları istifadə etmək olmaz
+})()
