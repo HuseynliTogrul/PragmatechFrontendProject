0b0ce8581eec88cd9d5a8c61303a08cbb2065bf4
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-03-30 23:12:27 +0400

    task2

--- JsDataStructures/tv02.js
+++ JsDataStructures/tv02.js
@@ -1,14 +1,19 @@
-// inputs
-
-let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban'];
-let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade'];
-let ages = [23, 34, 40, 58, 60];
-let students = {
-    adlar: names,
-    soyadlar: surnames,
-    yaslar: ages
+// inputs 
+ 
+let names = ['Eli', 'Ehmed', 'Sara', 'Sevda', 'Qurban']; 
+let surnames = ['Piriyev', 'Memmedov', 'Qurbanova', 'Ezizova', 'Sadiqzade']; 
+let ages = [23, 34, 40, 58, 60]; 
+let students = { 
+    adlar: names, 
+    soyadlar: surnames, 
+    yaslar: ages 
+} 
+ 
+//expected output 
+// Ad: Eli,Soyad:Piriyev,Yas:23 
+// Ad: Ehmed,Soyad:Memmedov,Yas:34 
+ 
+ 
+for(i=0;i < 5;i++){ 
+    console.log(`Ad: ${students.adlar[i]},Soyad:${students.soyadlar[i]},Yas:${students.yaslar[i]}`) 
 }
-
-//expected output
-// Ad: Eli,Soyad:Piriyev,Yas:23
-// Ad: Ehmed,Soyad:Memmedov,Yas:34
