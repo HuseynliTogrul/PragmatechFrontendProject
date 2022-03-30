759af1a4c59d07881853afc59a5b8c7919577a95
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-03-30 23:13:22 +0400

    task3

--- JsDataStructures/tv03.js
+++ JsDataStructures/tv03.js
@@ -1,27 +1,32 @@
-// inputs
-
-let students = [{
-    ad: 'Eli',
-    soyad: 'Piriyev',
-    yas: 23
-}, {
-    ad: 'Ehmed',
-    soyad: 'Memmedov',
-    yas: 34
-}, {
-    ad: 'Sara',
-    soyad: 'Qurbanova',
-    yas: 40
-}, {
-    ad: 'Sevda',
-    soyad: 'Ezizova',
-    yas: 58
-}, {
-    ad: 'Qurban',
-    soyad: 'Sadiqzade',
-    yas: 60
-}]
-
-//expected output
-// Ad: Eli,Soyad:Piriyev,Yas:23
-// Ad: Ehmed,Soyad:Memmedov,Yas:34
+// inputs 
+ 
+let students = [{ 
+    ad: 'Eli', 
+    soyad: 'Piriyev', 
+    yas: 23 
+}, { 
+    ad: 'Ehmed', 
+    soyad: 'Memmedov', 
+    yas: 34 
+}, { 
+    ad: 'Sara', 
+    soyad: 'Qurbanova', 
+    yas: 40 
+}, { 
+    ad: 'Sevda', 
+    soyad: 'Ezizova', 
+    yas: 58 
+}, { 
+    ad: 'Qurban', 
+    soyad: 'Sadiqzade', 
+    yas: 60 
+}] 
+ 
+//expected output 
+// Ad: Eli,Soyad:Piriyev,Yas:23 
+// Ad: Ehmed,Soyad:Memmedov,Yas:34 
+ 
+ 
+for(i=0;i < 5;i++){ 
+    console.log(`Ad: ${students[i].ad},Soyad:${students[i].soyad},Yas:${students[i].yas}`) 
+}
