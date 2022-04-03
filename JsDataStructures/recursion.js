6917c471d2d82d0887d493beffd030e0bcead5b4
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-04-03 15:21:57 +0400

    tasks

--- JsDataStructures/recursion.js
+++ JsDataStructures/recursion.js
@@ -2,6 +2,6 @@
     // sadəcə burda kod yazmağınıza icaze verilir
     // teleb olunan 1-100 arasındakı ədədlərin ekrana cap edilməsini təmin etməkdir
     // while for və digər loop expressionları istifadə etmək olmaz
-
+    
     Array.from({length: 100},(_,x) => console.log(x+1))
-})()
+})() 
