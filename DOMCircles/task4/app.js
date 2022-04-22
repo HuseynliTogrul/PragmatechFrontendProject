795292d60bc458d07984c95bf591deb4314e18b3
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-04-22 22:39:26 +0400

    task 4 end

--- DOMCircles/task4/app.js
+++ DOMCircles/task4/app.js
@@ -1,4 +1,18 @@
-function randomCirclePlacement() {
-    // radiusu 5px olan bir dairə yaradın
-    // funksiya hər dəfə icra olunanda yaradılan dairə body elementinin daxilində təsadufi bir yerdə görünməlidir
-  }
+// function randomCirclePlacement() {
+//      radiusu 5px olan bir dairə yaradın
+//      funksiya hər dəfə icra olunanda yaradılan dairə body elementinin daxilində təsadufi bir yerdə görünməlidir
+//   }
+
+function randomCirclePlacement(_radius) {
+  let circle = document.createElement('div');
+  let randX = Math.random() * 1000;
+  let randY = Math.random() * 1000;
+  circle.style.width = `${_radius*2}px`;
+  circle.style.height = `${_radius*2}px`;
+  circle.style.backgroundColor = "blue";
+  circle.className = 'daire';
+  circle.style.top = `${randX}px`;
+  circle.style.left = `${randY}px`;
+  document.body.append(circle);
+}
+randomCirclePlacement(5)
