fa887b9a5252bea2b61d3a3025414f52f7fbef46
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-04-22 21:41:39 +0400

    task1 end

--- DOMCircles/task1/app.js
+++ DOMCircles/task1/app.js
@@ -1,15 +1,18 @@
+// function createCircle(_selector, _radius, _backgroundColor) {
+//     Verilən parametrlərə əsasən dairə yaradan metod yazın
+//     _selector -> yaradılan dairənin html daxilində hansı elementin daxilinə əlavə ediləcəyini müəyyən edəcək.Yəni '.box' argumenti daxil edildiyi zaman yaranan dairə .box adlı elementin daxilinə əlavə ediləcək
+//     _radius->yaradılacaq dairənin radiusu daxil ediləcək ədəd olaraq
+//     _backgroundColor->yaradılacaq dairənin arxafon rəngi daxil ediləcək string olaraq
+//   }
+
 function createCircle(_selector, _radius, _backgroundColor) {
-    // Verilən parametrlərə əsasən dairə yaradan metod yazın
-    // _selector -> yaradılan dairənin html daxilində hansı elementin daxilinə əlavə ediləcəyini müəyyən edəcək.Yəni '.box' argumenti daxil edildiyi zaman yaranan dairə .box adlı elementin daxilinə əlavə ediləcək
-    // _radius->yaradılacaq dairənin radiusu daxil ediləcək ədəd olaraq
-    // _backgroundColor->yaradılacaq dairənin arxafon rəngi daxil ediləcək string olaraq
+  let circle = document.createElement('div');
+  circle.style.width = `${_radius*2}px`;
+  circle.style.height = `${_radius*2}px`;
+  circle.style.backgroundColor = `${_backgroundColor}`;
+  circle.className = 'daire';
+  document.body.append(circle)
+  document.querySelector(`${_selector}`).appendChild(circle);
 
-    let div = document.createElement('div');
-    div.style.width = `${_radius}px`;
-    div.style.height = `${_radius}px`;
-    div.style.borderRadius = '50%';
-    div.style.border = "1p solid black";
-    div.style.background = _backgroundColor;
-    let container = document.querySelector(selector)
-  }
-
+}
+createCircle('container',100, 'lightblue');
