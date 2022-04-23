078f5efa5e406530366e704225a17624a10edbfb
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-04-23 22:34:08 +0400

    tasks end

--- DOMCircles/task5/app.js
+++ DOMCircles/task5/app.js
@@ -3,4 +3,4 @@ function createPoolTable() {
     // ağ dairə xaricində geri qalan topların daxilində nömrələri yazılacaq
     // bilyard masasI görüntüsü üçün [link](https://upload.wikimedia.org/wikipedia/commons/5/5e/American-style_pool_table_diagram_%28empty%29.png)-dəki arxafon şəklindən istifadə edin.
     // yaradılan toplar random olaraq görüntüsü verilən bilyard masası üzərində random yerlərdə yerləşməlidir.
-  }
+  } s
