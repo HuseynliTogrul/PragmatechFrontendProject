2870230492b4168ec602d942caf488623c3a86e0
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-04-28 00:48:39 +0400

    dark-light

---
+++ BackgroundSwitcher/app.js
@@ -0,0 +1,11 @@
+// function clickButton(){
+//     Light.style.color = "#212150";
+//     Dark.style.color = "#cbcaca";
+// }
+
+function clickButton(){
+    Light.style.color = "rgb(82 79 79)";
+    Dark.style.color = "white";
+    container.style.backgroundColor = "#131326"
+    darkLight.style.backgroundColor = "#080918"
+}
