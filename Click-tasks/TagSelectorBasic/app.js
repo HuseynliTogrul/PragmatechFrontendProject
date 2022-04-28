07dee193ddb05acd44212ece1c4f196891749224
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-04-28 17:46:46 +0400

    click

---
+++ Click-tasks/TagSelectorBasic/app.js
@@ -0,0 +1,14 @@
+function clickItem(){
+    h1.style.backgroundColor = "blue"
+    items.style.color = "red"
+}
+
+
+// function clickButton(){
+//     Light.style.color = "rgb(82 79 79)";
+//     Dark.style.color = "white";
+//     container.style.backgroundColor = "#131326";
+//     darkLight.style.backgroundColor = "#080918";
+//     border.style.left = `${80}px`;
+//     border.style.transition = "all .3s linear";
+// }
