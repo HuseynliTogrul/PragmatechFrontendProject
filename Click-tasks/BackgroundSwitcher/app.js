4be1eaaf3a6df255e967a6c7f5c31b325d78c354
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-04-28 14:51:55 +0400

    light-dark

---
+++ Click-tasks/BackgroundSwitcher/app.js
@@ -0,0 +1,14 @@
+// function clickButton(){
+//     Light.style.color = "#212150";
+//     Dark.style.color = "#cbcaca";
+// } 
+
+
+function clickButton(){
+    Light.style.color = "rgb(82 79 79)";
+    Dark.style.color = "white";
+    container.style.backgroundColor = "#131326";
+    darkLight.style.backgroundColor = "#080918";
+    border.style.left = `${80}px`;
+    border.style.transition = "all .3s linear";
+}
