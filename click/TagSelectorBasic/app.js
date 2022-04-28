0731fe67f92e8502b8b392af69a28df4a16059c5
HuseynliTogrul <huseynlitogrul2002@gmail.com>
2022-04-28 19:03:55 +0400

    click.

---
+++ click/TagSelectorBasic/app.js
@@ -0,0 +1,30 @@
+function First1() {
+    item1.style.backgroundColor = "lightblue";
+}
+function First2() {
+    item2.style.backgroundColor = "lightblue";
+}
+function First3() {
+    item3.style.backgroundColor = "lightblue";
+}
+function Middle4() {
+    item4.style.backgroundColor = "lightblue";
+}
+function Middle5() {
+    item5.style.backgroundColor = "lightblue";
+}
+function Middle6() {
+    item6.style.backgroundColor = "lightblue";
+}
+function Last7() {
+    item7.style.backgroundColor = "lightblue";
+}
+function Last8() {
+    item8.style.backgroundColor = "lightblue";
+}
+function Last9() {
+    item9.style.backgroundColor = "lightblue";
+}
+function Last0() {
+    item0.style.backgroundColor = "lightblue";
+}
